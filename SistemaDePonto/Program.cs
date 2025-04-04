06fdca2fdad6776444b5811dc0150a7f06e06334
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2025-04-03 21:04:51 -0300

    Add API and Infrastructure authentication business

--- SistemaDePonto/Program.cs
+++ SistemaDePonto/Program.cs
@@ -1,9 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SistemaDePonto.Application.Interfaces;
+using SistemaDePonto.Infrastructure.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        string projectId = Environment.GetEnvironmentVariable("SistemaDePonto__FirebaseProjectId")
+        ?? throw new ArgumentNullException("Variável de ambiente 'SistemaDePonto__FirebaseProjectId' não encontrada.");
+
+        options.Authority = $"https://securetoken.google.com/{projectId}";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = $"https://securetoken.google.com/{projectId}",
+
+            ValidateAudience = true,
+            ValidAudience = projectId,
+
+            ValidateLifetime = true,
+        };
+    });
+
+// Add infrastructure to DI
+builder.Services.AddScoped<ICurrentUser, CurrentUser>();
+
+// Add application to DI
 
 var app = builder.Build();
 
@@ -16,8 +45,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
